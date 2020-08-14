4393d19816265ee3bf0da3d5ec2a44f941024ab8
wangjingfei <982975913@qq.com>
2020-08-14 17:56:05 +0800

    wjf

--- src/components/Activity.vue
+++ src/components/Activity.vue
@@ -3,73 +3,78 @@
     <h2 class="title">{{$t("message.*首页活动")}}</h2>
     <!-- <span>{{activitys}}</span> -->
     <el-row :gutter="20">
-        <el-col :span="6" v-for="(o) in activitys" :key="o">
-            <span>{{o}}</span>
-            <!-- <el-card shadow="hover" :body-style="{ padding: '5px',borderRadius:'20px' }">
+      <el-col
+        :span="6"
+        v-for="(o) in activitys"
+        :key="o"
+        style="text-align:center;background: linear-gradient(to top right, #FF9500, #ff5000);line-height: 45px;"
+      >
+        <span style="font-size: 18px;font-weight: 700;color:#fff">{{o}}</span>
+        <!-- <el-card shadow="hover" :body-style="{ padding: '5px',borderRadius:'20px' }">
             <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png" class="image">
-            </el-card> -->
-        </el-col>
+        </el-card>-->
+      </el-col>
     </el-row>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
+  name: "HelloWorld",
+  data() {
     return {
-        currentDate: new Date()
-    }
+      currentDate: new Date(),
+    };
   },
-  computed:{
-    activitys(){
-      var a=[]
-      
-      this.$store.state.activitys.forEach(element => {
+  computed: {
+    activitys() {
+      var a = [];
+
+      this.$store.state.activitys.forEach((element) => {
         // console.log(element)
-          if(element != "首页活动"){
-            a.push(element)
-          }
+        if (element != "首页活动") {
+          a.push(element);
+        }
       });
       // console.log('默认',a)
-      return a
-      }
+      return a;
+    },
   },
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
-.title{
+.title {
   margin-bottom: 15px;
 }
 .time {
-font-size: 13px;
-color: #999;
+  font-size: 13px;
+  color: #999;
 }
 
 .bottom {
-margin-top: 13px;
-line-height: 12px;
+  margin-top: 13px;
+  line-height: 12px;
 }
 
 .button {
-padding: 0;
-float: right;
+  padding: 0;
+  float: right;
 }
 
 .image {
-width: 100%;
-display: block;
+  width: 100%;
+  display: block;
 }
 
 .clearfix:before,
 .clearfix:after {
-    display: table;
-    content: "";
+  display: table;
+  content: "";
 }
 
 .clearfix:after {
-    clear: both
+  clear: both;
 }
 </style>
