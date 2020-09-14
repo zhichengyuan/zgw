38b8fd2d2dba7a65e3ca210c1936cd9118d88d5e
wangjingfei <982975913@qq.com>
2020-09-14 15:23:04 +0800

    wjf

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -4,10 +4,21 @@ import VueI18n from 'vue-i18n'
 import messages from './langs'
 Vue.use(VueI18n)
 
-let locale1="ru";
-
+let locale1="";
+var curlang = ""
+var apptype = navigator.appName
+if(apptype = "Netscape"){
+    curlang = navigator.language
+}else{
+    curlang = navigator.userLanguage
+}
+if (curlang != "zh-cn" && curlang != "zh-CN") {
+    curlang = "ru"
+}
+locale1 = curlang
+console.log(locale1)
 let i18n = new VueI18n({
-    locale:  locale1,
+    locale:  locale1 == "ru"?"ru":"cn",
     messages,
 })
 locale.i18n((key, value) => i18n.t(key, value)) //兼容element
